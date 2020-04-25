33b00f66851f9a106084afffb14c8ec6e01a4c88
postables <postables@rtradetechnologies.com>
2020-04-24 19:20:16 -0700

    rename first_boot, and proper install i2p script

---
+++ scripts/install_i2p.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo apt-add-repository ppa:i2p-maintainers/i2p
+sudo apt-get update
+sudo apt-get install i2p i2p-keyring
+# use this to setup the i2p service to start at boot
+sudo dpkg-reconfigure i2p
